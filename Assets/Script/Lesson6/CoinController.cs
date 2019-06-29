27021366b842c69d80c5005e3b7fd4d5bdcdda5b
mentor-matsumoto-ka <w07130km@hotmail.co.jp>
2019-06-29 20:24:02 +0900

    進捗コミット。

--- Assets/Script/Lesson6/CoinController.cs
+++ Assets/Script/Lesson6/CoinController.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinController : MonoBehaviour
+namespace Leasson7
 {
-    // Use this for initialization
-    void Start()
+    public class CoinController : MonoBehaviour
     {
-        //回転を開始する角度を設定
-        this.transform.Rotate(0, Random.Range(0, 360), 0);
-    }
+        // Use this for initialization
+        void Start()
+        {
+            //回転を開始する角度を設定
+            this.transform.Rotate(0, Random.Range(0, 360), 0);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //回転
-        this.transform.Rotate(0, 3, 0);
+        // Update is called once per frame
+        void Update()
+        {
+            //回転
+            this.transform.Rotate(0, 3, 0);
+        }
     }
 }
