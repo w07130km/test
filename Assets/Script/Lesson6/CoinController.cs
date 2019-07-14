e336de964c443cd221cb39739840a426401ce4ff
mentor-matsumoto-ka <w07130km@hotmail.co.jp>
2019-07-14 14:56:49 +0900

    Revert "進捗コミット。"
    This reverts commit 27021366b842c69d80c5005e3b7fd4d5bdcdda5b.

--- Assets/Script/Lesson6/CoinController.cs
+++ Assets/Script/Lesson6/CoinController.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Leasson7
+public class CoinController : MonoBehaviour
 {
-    public class CoinController : MonoBehaviour
+    // Use this for initialization
+    void Start()
     {
-        // Use this for initialization
-        void Start()
-        {
-            //回転を開始する角度を設定
-            this.transform.Rotate(0, Random.Range(0, 360), 0);
-        }
+        //回転を開始する角度を設定
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
-            //回転
-            this.transform.Rotate(0, 3, 0);
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        //回転
+        this.transform.Rotate(0, 3, 0);
     }
 }
--- Assets/Script/Lesson6/UnityChanController.cs
+++ Assets/Script/Lesson6/UnityChanController.cs
@@ -1,147 +1,144 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Leasson7
+
+public class UnityChanController : MonoBehaviour
 {
-    public class UnityChanController : MonoBehaviour
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+    //Unityちゃんを移動させるコンポーネントを入れる（追加）
+    private Rigidbody myRigidbody;
+    //前進するための力（追加）
+    private float forwardForce = 800.0f;
+    //左右に移動するための力（追加）
+    private float turnForce = 500.0f;
+    //左右の移動できる範囲（追加）
+    private float movableRange = 3.4f;
+    //ジャンプするための力（追加）
+    private float upForce = 700.0f;
+    //動きを減速させる係数（追加）
+    private float coefficient = 0.95f;
+
+    //ゲーム終了の判定（追加）
+    private bool isEnd = false;
+
+    //左ボタン押下の判定（追加）
+    private bool isLButtonDown = false;
+    //右ボタン押下の判定（追加）
+    private bool isRButtonDown = false;
+
+    // Use this for initialization
+    void Start()
     {
-        //アニメーションするためのコンポーネントを入れる
-        private Animator myAnimator;
-        //Unityちゃんを移動させるコンポーネントを入れる（追加）
-        private Rigidbody myRigidbody;
-        //前進するための力（追加）
-        private float forwardForce = 800.0f;
-        //左右に移動するための力（追加）
-        private float turnForce = 500.0f;
-        //左右の移動できる範囲（追加）
-        private float movableRange = 3.4f;
-        //ジャンプするための力（追加）
-        private float upForce = 700.0f;
-        //動きを減速させる係数（追加）
-        private float coefficient = 0.95f;
 
-        //ゲーム終了の判定（追加）
-        private bool isEnd = false;
+        //アニメータコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
 
-        //左ボタン押下の判定（追加）
-        private bool isLButtonDown = false;
-        //右ボタン押下の判定（追加）
-        private bool isRButtonDown = false;
+        //走るアニメーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
 
-        // Use this for initialization
-        void Start()
+        //Rigidbodyコンポーネントを取得（追加）
+        this.myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ゲーム終了ならUnityちゃんの動きを減衰する（追加）
+        if (this.isEnd)
         {
-
-            //アニメータコンポーネントを取得
-            this.myAnimator = GetComponent<Animator>();
-
-            //走るアニメーションを開始
-            this.myAnimator.SetFloat("Speed", 1);
-
-            //Rigidbodyコンポーネントを取得（追加）
-            this.myRigidbody = GetComponent<Rigidbody>();
+            this.forwardForce *= this.coefficient;
+            this.turnForce *= this.coefficient;
+            this.upForce *= this.coefficient;
+            this.myAnimator.speed *= this.coefficient;
         }
 
-        // Update is called once per frame
-        void Update()
+        //Unityちゃんに前方向の力を加える（追加）
+        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+
+        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
+        if ((Input.GetKey(KeyCode.LeftArrow) || this.isLButtonDown) && -this.movableRange < this.transform.position.x)
         {
-            //ゲーム終了ならUnityちゃんの動きを減衰する（追加）
-            if (this.isEnd)
-            {
-                this.forwardForce *= this.coefficient;
-                this.turnForce *= this.coefficient;
-                this.upForce *= this.coefficient;
-                this.myAnimator.speed *= this.coefficient;
-            }
-
-            //Unityちゃんに前方向の力を加える（追加）
-            this.myRigidbody.AddForce(new Vector3(0, 0, this.forwardForce));
-            //this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
-
-            //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
-            if ((Input.GetKey(KeyCode.LeftArrow) || this.isLButtonDown) && -this.movableRange < this.transform.position.x)
-            {
-                //左に移動
-                this.myRigidbody.AddForce(-this.turnForce, 0, 0);
-            }
-            else if ((Input.GetKey(KeyCode.RightArrow) || this.isRButtonDown) && this.transform.position.x < this.movableRange)
-            {
-                //右に移動
-                this.myRigidbody.AddForce(this.turnForce, 0, 0);
-            }
-
-            //Jumpステートの場合はJumpにfalseをセットする（追加）
-            if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
-            {
-                this.myAnimator.SetBool("Jump", false);
-            }
-
-            //ジャンプしていない時にスペースが押されたらジャンプする（追加）
-            if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
-            {
-                //ジャンプアニメを再生（追加）
-                this.myAnimator.SetBool("Jump", true);
-                //Unityちゃんに上方向の力を加える（追加）
-                this.myRigidbody.AddForce(this.transform.up * this.upForce);
-            }
+            //左に移動
+            this.myRigidbody.AddForce(-this.turnForce, 0, 0);
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow) || this.isRButtonDown) && this.transform.position.x < this.movableRange)
+        {
+            //右に移動
+            this.myRigidbody.AddForce(this.turnForce, 0, 0);
         }
 
-        //トリガーモードで他のオブジェクトと接触した場合の処理（追加）
-        void OnTriggerEnter(Collider other)
+        //Jumpステートの場合はJumpにfalseをセットする（追加）
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
         {
-            Debug.Log("other.gameObject.tag:" + other.gameObject.tag);
-            //障害物に衝突した場合（追加）
-            if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
-            {
-                this.isEnd = true;
-            }
-
-            //ゴール地点に到達した場合（追加）
-            if (other.gameObject.tag == "GoalTag")
-            {
-                this.isEnd = true;
-            }
-
-            //コインに衝突した場合（追加）
-            if (other.gameObject.tag == "CoinTag")
-            {
-                //パーティクルを再生（追加）
-                GetComponent<ParticleSystem>().Play();
-                //接触したコインのオブジェクトを破棄（追加）
-                Destroy(other.gameObject);
-            }
+            this.myAnimator.SetBool("Jump", false);
         }
 
-        //ジャンプボタンを押した場合の処理（追加）
-        public void GetMyJumpButtonDown()
+        //ジャンプしていない時にスペースが押されたらジャンプする（追加）
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
         {
-            if (this.transform.position.y < 0.5f)
-            {
-                this.myAnimator.SetBool("Jump", true);
-                this.myRigidbody.AddForce(this.transform.up * this.upForce);
-            }
+            //ジャンプアニメを再生（追加）
+            this.myAnimator.SetBool("Jump", true);
+            //Unityちゃんに上方向の力を加える（追加）
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
+        }
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理（追加）
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("other.gameObject.tag:" + other.gameObject.tag);
+        //障害物に衝突した場合（追加）
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
+        {
+            this.isEnd = true;
         }
 
-        //左ボタンを押し続けた場合の処理（追加）
-        public void GetMyLeftButtonDown()
+        //ゴール地点に到達した場合（追加）
+        if (other.gameObject.tag == "GoalTag")
         {
-            this.isLButtonDown = true;
-        }
-        //左ボタンを離した場合の処理（追加）
-        public void GetMyLeftButtonUp()
-        {
-            this.isLButtonDown = false;
+            this.isEnd = true;
         }
 
-        //右ボタンを押し続けた場合の処理（追加）
-        public void GetMyRightButtonDown()
+        //コインに衝突した場合（追加）
+        if (other.gameObject.tag == "CoinTag")
         {
-            this.isRButtonDown = true;
+            //パーティクルを再生（追加）
+            GetComponent<ParticleSystem>().Play();
+            //接触したコインのオブジェクトを破棄（追加）
+            Destroy(other.gameObject);
         }
-        //右ボタンを離した場合の処理（追加）
-        public void GetMyRightButtonUp()
+    }
+
+    //ジャンプボタンを押した場合の処理（追加）
+    public void GetMyJumpButtonDown()
+    {
+        if (this.transform.position.y < 0.5f)
         {
-            this.isRButtonDown = false;
+            this.myAnimator.SetBool("Jump", true);
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
         }
+    }
+
+    //左ボタンを押し続けた場合の処理（追加）
+    public void GetMyLeftButtonDown()
+    {
+        this.isLButtonDown = true;
+    }
+    //左ボタンを離した場合の処理（追加）
+    public void GetMyLeftButtonUp()
+    {
+        this.isLButtonDown = false;
+    }
+
+    //右ボタンを押し続けた場合の処理（追加）
+    public void GetMyRightButtonDown()
+    {
+        this.isRButtonDown = true;
+    }
+    //右ボタンを離した場合の処理（追加）
+    public void GetMyRightButtonUp()
+    {
+        this.isRButtonDown = false;
     }
 }
