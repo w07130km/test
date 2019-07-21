61c636911e7f8bdc69e1d40e60e179cb54b5e709
mentor-matsumoto-ka <w07130km@hotmail.co.jp>
2019-07-21 19:13:07 +0900

    Revert "Revert "進捗コミット。""
    This reverts commit e336de964c443cd221cb39739840a426401ce4ff.

---
+++ Assets/Script/Lesson8/UnityChanController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Leasson9
+{
+    public class UnityChanController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // マウスがクリックされた場合
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Animatorコンポーネントを取得し、"jumpTrigger"をtrueにする
+                GetComponent<Animator>().SetTrigger("jumpTrigger");
+            }
+        }
+    }
+}
